ed4e1604ae3f85409824315754011d5f180bcc6e
anelisevaz.anelise <anevaz1@gmail.com>
2022-02-09 23:06:11 -0300

    Inserindo o teste

--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
@@ -9,7 +12,10 @@ public class TesteGoogle {
 	public void teste() {
 		//System.setProperty("webdriver.gecko.driver", "/Users/Anelise/Downloads/drivers/geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("http://www.google.com");
 		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
 	}
 }
